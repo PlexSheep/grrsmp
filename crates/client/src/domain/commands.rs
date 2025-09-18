82107c018985734f1167dee050d3cc0b3f4b69e8
Christoph J. Scherr <software@cscherr.de>
2025-09-18 23:21:22 +0200

    feat: application domain structure, commands and events

---
+++ crates/client/src/domain/commands.rs
@@ -0,0 +1,41 @@
+use std::{fmt::Display, net::SocketAddr, sync::Arc};
+
+use ed25519_dalek::VerifyingKey;
+use sremp_core::chat::messages::SharedMessage;
+
+use crate::identity::{UserIdentity, format_key};
+
+#[derive(Debug, Clone)]
+#[allow(clippy::large_enum_variant)]
+pub enum UiCommand {
+    SetIdentity(UserIdentity),
+    SendMessage(VerifyingKey, SharedMessage),
+    StartListener(SocketAddr),
+    StopListener,
+    Connect(SocketAddr),
+    Disconnect(SocketAddr),
+}
+
+impl Display for UiCommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Connect(addr) => format!("Connect to {addr}"),
+                Self::Disconnect(addr) => format!("Disconnect from {addr}"),
+                Self::SendMessage(id, _msg) => format!("Send Message to {}", format_key(&id)),
+                Self::StartListener(addr) =>
+                    format!("Start listening for incoming connection on {addr}"),
+                Self::StopListener => "Stop listening for incoming connections".to_string(),
+                Self::SetIdentity(id) => {
+                    format!(
+                        "Set working copy of user identity to {} ({})",
+                        format_key(&id.identity.public_key),
+                        id.identity.username()
+                    )
+                }
+            }
+        )
+    }
+}
--- crates/client/src/domain/mod.rs
+++ crates/client/src/domain/mod.rs
@@ -1,9 +1,37 @@
-pub mod known_identities;
-
+use async_channel::{Receiver, Sender};
 use serde::{Deserialize, Serialize};
+
+mod commands;
+mod events;
+
+pub use commands::UiCommand;
+pub use events::UiEvent;
+
+pub mod known_identities;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 pub struct ApplicationDomain {
     pub known_identities: KnownIdentities,
     pub chats: HashMap<VerifyingKey, Chat>,
+}
+
+impl ApplicationDomain {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    fn to_sync(self) -> NetworkDomainSync {
+        Arc::new(RwLock::new(self))
+    }
+
+    pub fn run(
+        self,
+        net_command_channel: Sender<NetworkCommand>,
+        net_event_channel: Receiver<NetworkEvent>,
+        ui_command_channel: Receiver<UiCommand>,
+        ui_event_channel: Sender<UiEvent>,
+        rt: &mut tokio::runtime::Runtime,
+    ) -> CoreResult<()> {
+        todo!()
+    }
 }
