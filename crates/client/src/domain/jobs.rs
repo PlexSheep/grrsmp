d038d59052bbaf6bfc8f7394874dc5372ba111d9
Christoph J. Scherr <software@cscherr.de>
2025-09-19 15:42:52 +0200

    feat: client processes ui command

--- crates/client/src/domain/jobs.rs
+++ crates/client/src/domain/jobs.rs
@@ -1,1 +1,29 @@
-pub(super) async fn job_handle_app_commands() {}
+use async_channel::Sender;
+use sremp_core::{
+    domain::{NetworkCommand, NetworkEvent},
+    error::CoreResult,
+};
+
+use crate::domain::{ClientDomain, UiCommand, UiEvent};
+
+impl ClientDomain {
+    pub(super) async fn process_ui_command(
+        &mut self,
+        command: UiCommand,
+        _command_tx: Sender<NetworkCommand>,
+    ) -> CoreResult<UiEvent> {
+        log::info!("Processing Ui Command: {command}");
+        let event = match command {
+            _ => todo!(),
+        };
+        log::info!("Event emerged after processing the Ui Command: {event}");
+        Ok(event)
+    }
+    pub(super) async fn process_net_event(&mut self, event: NetworkEvent) -> CoreResult<()> {
+        log::info!("Processing Net Event: {event}");
+        match event {
+            _ => todo!(),
+        }
+        Ok(())
+    }
+}
--- crates/client/src/domain/mod.rs
+++ crates/client/src/domain/mod.rs
@@ -3,39 +3,43 @@ use std::{collections::HashMap, sync::Arc};
 use async_channel::{Receiver, Sender};
 use ed25519_dalek::VerifyingKey;
 use serde::{Deserialize, Serialize};
+use tokio::{sync::RwLock, task::JoinHandle};
 
 mod commands;
 mod events;
+mod jobs;
 
 pub use commands::UiCommand;
 pub use events::UiEvent;
 use sremp_core::{
     chat::Chat,
     domain::{NetworkCommand, NetworkEvent},
-    error::CoreResult,
+    error::CoreError,
     identity::UserIdentity,
 };
-use tokio::{sync::RwLock, task::JoinHandle};
 
 use crate::domain::known_identities::KnownIdentities;
+use crate::error::ClientResult;
 
 pub mod known_identities;
 
-pub type ApplicationDomainSync = Arc<RwLock<ApplicationDomain>>;
+pub type ClientDomainSync = Arc<RwLock<ClientDomain>>;
+
+const JOB_ITERATION_INTERVAL_MS: u64 = 30;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
-pub struct ApplicationDomain {
+pub struct ClientDomain {
     pub(crate) known_identities: KnownIdentities,
     pub(crate) chats: HashMap<VerifyingKey, Chat>,
     pub(crate) user_identity: Option<UserIdentity>,
 }
 
-impl ApplicationDomain {
+impl ClientDomain {
     pub fn new() -> Self {
         Self::default()
     }
 
-    fn to_sync(self) -> ApplicationDomainSync {
+    fn into_sync(self) -> ClientDomainSync {
         Arc::new(RwLock::new(self))
     }
 
@@ -45,8 +49,30 @@ impl ApplicationDomain {
         net_event_channel: Receiver<NetworkEvent>,
         ui_command_channel: Receiver<UiCommand>,
         ui_event_channel: Sender<UiEvent>,
-    ) -> CoreResult<()> {
-        todo!()
+    ) -> ClientResult<()> {
+        let ssy = self.into_sync();
+        loop {
+            let this = ssy.read().await;
+            tokio::select! {
+                cmd = ui_command_channel.recv() => {
+                    drop(this);
+                    let cmd = cmd.map_err(CoreError::from)?;
+                    let event = ssy.write().await.process_ui_command(
+                        cmd,
+                        net_command_channel.clone()
+                    ).await?;
+                    ui_event_channel.send(event).await?;
+                },
+                evt = net_event_channel.recv() => {
+                    drop(this);
+                    let evt = evt.map_err(CoreError::from)?;
+                    ssy.write().await.process_net_event(evt).await?;
+                },
+                _ = tokio::time::sleep(tokio::time::Duration::from_millis(JOB_ITERATION_INTERVAL_MS)) => {
+                    continue;
+                }
+            }
+        }
     }
 
     pub fn start(
@@ -56,7 +82,7 @@ impl ApplicationDomain {
         ui_command_channel: Receiver<UiCommand>,
         ui_event_channel: Sender<UiEvent>,
         rt: &mut tokio::runtime::Runtime,
-    ) -> CoreResult<JoinHandle<CoreResult<()>>> {
+    ) -> ClientResult<JoinHandle<ClientResult<()>>> {
         let handle = rt.spawn(async {
             self.run(
                 net_command_channel,
